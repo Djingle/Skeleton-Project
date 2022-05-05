e567985e81dfc237c85dcd77526210f24a4e9aef
rajendiran <vinojan.rajendiran@etu.unistra.fr>
2022-05-05 11:49:09 +0200

    Add missing test output files

--- src/SkeletonTest.cpp
+++
@@ -1,6 +0,0 @@
-///
-/// \file SkeletonTest.cpp
-/// \brief Source file of SkeletonTest
-/// \author EMERY Bryan, HORNY Gregory, LABAYE Paul, LAURENT Titouan, RAJENDIRAN Vinojan
-/// \version 1.0.0
-///
--- src/SkeletonTest.h
+++
@@ -1,6 +0,0 @@
-///
-/// \file SkeletonTest.h
-/// \brief Header file of SkeletonTest
-/// \author EMERY Bryan, HORNY Gregory, LABAYE Paul, LAURENT Titouan, RAJENDIRAN Vinojan
-/// \version 1.0.0
-///
